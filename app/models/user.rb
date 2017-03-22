9c2310bff445b7a9d311819dda2c786b7d8a648d
blackknight75 <devbox4714@gmail.com>
2017-03-21 18:45:08 -0600

    add basic style framework

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+
+  def self.find_user(authorized_data, access_token)
+    user = User.find_or_create_by(uid: authorized_data[:id])
+    user.set_attributes(authorized_data, access_token)
+    user
+  end
+
+  def set_attributes(authorized_data, access_token)
+    self.username = authorized_data[:login]
+    self.token = access_token
+    self.email = authorized_data[:email]
+    self.image = authorized_data[:avatar_url]
+    self.repos_url = authorized_data[:repos_url]
+    self.public_repos = authorized_data[:public_repos]
+    self.public_gists = authorized_data[:public_gists]
+    self.starred_url = authorized_data[:starred_url]
+    self.followers_url = authorized_data[:followers_url]
+    self.followers = authorized_data[:followers]
+    self.following_url = authorized_data[:following_url]
+    self.following = authorized_data[:following]
+    self.save
+  end
+end
