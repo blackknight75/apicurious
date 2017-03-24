5ff4481bf2a4076a5a84c19da1a0d87ac39d7782
blackknight75 <devbox4714@gmail.com>
2017-03-23 18:07:29 -0600

    add commits and style to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
 before_action :authorize!
   def index
     @starred_repos_count = Starred.all(current_user).count
+    @commits_by_repo = Commit.all(current_user).first(5)
+    @repos = Repo.all(current_user).first(6)
   end
 end
